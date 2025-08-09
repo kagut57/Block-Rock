ffafaaf5e040f22a08d6c7c7cabfefff8c2d14cf
kagut57 <156945786+kagut57@users.noreply.github.com>
2025-08-09 22:16:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import datetime
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ChatJoinRequest
